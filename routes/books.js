82fba30ce77bc4e9c71c7a7dace51f509890ef9a
jessebsawyer <46061645+jessebsawyer@users.noreply.github.com>
2020-09-10 20:23:13 -0400

    update

--- routes/books.js
+++ routes/books.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../config/database');
 const Book = require('../models/Book');
 
+// Show All Books
 router.get('/', (req, res) => 
 Book.findAll()
     .then(book => {
@@ -13,5 +14,31 @@ Book.findAll()
     })
     .catch(err => console.log(err))
 );
+
+// Render New Book Form
+router.get('/new', (req, res) => res.render('new-book'))
+
+// Render Update Book
+router.get('/:id', (req, res) => {
+    let id = req.params.id;
+    Book.findByPk(id)
+        .then(book => {
+            res.render('update-book', {
+                book
+            });
+        })
+        .catch(err => console.log(err))
+})
+
+// Update Book
+router.post('/:id', (req, res) => {
+    let id = req.params.id;
+    Book.findByPk(id)
+        .then(book => {
+            book.update()
+                .then(res.redirect('/books'))
+        })
+        .catch(err => console.log(err))
+})      
 
 module.exports = router;
